e2ca57c74c8581d0cc0f25d1946578a672d879c1
Marcin <kwiatekm4@gmail.com>
2021-06-22 16:08:45 +0200

    creating tasks to be performed

---
+++ my-app/src/Todo.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+class Todo extends React.Component {
+    state = {
+        elements: [
+            {id:'23424', title:'Zrobić zakupy'},
+            {id:'23334', title:'Zrobić pranie'},
+            {id:'23234', title:'Zmyć naczynia'},
+        ]
+    }
+    render() {
+        const elements = this.state.elements.map(e =>{
+            return <div className='task' key={e.id}>{e.title}</div>
+        })
+        return(
+            <div>
+                <div className="toDoTitle">Zadania do wykonania</div>
+                {elements}
+            </div>
+        )
+    }
+}
+
+export default Todo
