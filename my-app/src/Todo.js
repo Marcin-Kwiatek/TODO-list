42a60394a7d241b5795580afe46e646d4c04abf7
Marcin <kwiatekm4@gmail.com>
2021-06-26 12:53:43 +0200

    adding sorting and confirmation of deletion

--- my-app/src/Todo.js
+++ my-app/src/Todo.js
@@ -4,14 +4,15 @@ import TodoItem from './TodoItem'
 class Todo extends React.Component {
     state = {
         elements: [
-            {title:'Zrobić zakupy'},
-            {title:'Zrobić pranie'},
-            {title:'Zmyć naczynia'},
-            {title:'Odkurzyć'},
-            {title:'Pójść do pracy'},
-            {title:'Naprawić samochód'},
+            {title:'zrobić zakupy'},
+            {title:'zrobić pranie'},
+            {title:'zmyć naczynia'},
+            {title:'odkurzyć'},
+            {title:'pójść do pracy'},
+            {title:'naprawić samochód'},
         ],
-        inputValue: ""
+        inputValue: "",
+        sortButton: "Sortuj od A do Z"
     }
     render() {
         const elements = this.state.elements.map(e =>{
@@ -22,7 +23,7 @@ class Todo extends React.Component {
                 <div style={{clear:'both'}}>
                 <div className="toDoTitle">
                     Zadania do wykonania
-                    <button className='sortButton'>Sortuj od A do Z</button>
+                    <button className='sortButton' onClick={this.sortTask.bind(this)}>{this.state.sortButton}</button>
                     <input value={this.state.inputValue} onChange={this.inputHandler.bind(this)} style={{marginLeft:'50px'}}
                      type='text'></input>
                     <button style={{marginLeft:'5px'}} onClick={this.addTask.bind(this)}>Dodaj do listy</button>
@@ -38,19 +39,32 @@ class Todo extends React.Component {
         }
         const newElements = [item, ...this.state.elements]
         this.setState({elements: newElements})
+        this.setState({inputValue: ""})
     }
     inputHandler(event){
         const newValue = event.target.value
         this.setState({inputValue: newValue})
     }
     removeTask(title){
-        let filteredElements = []
-        for(let i=0; i<this.state.elements.length;i++){
-           if(this.state.elements[i].title!==title){
-               filteredElements.push(this.state.elements[i])
-           }
-        }
+        let filteredElements = this.state.elements.filter((element)=>element.title!==title)
         this.setState({elements: filteredElements})
+    }
+    sortTask(){
+        if(this.state.sortButton=== "Sortuj od A do Z"){
+        let sortedElements = this.state.elements.sort((a,b)=>{
+            if(b.title>a.title) return -1
+            else return 1
+        })
+        this.setState({elements: sortedElements})
+        this.setState({sortButton: "Sortuj od Z do A"})}
+
+        if(this.state.sortButton=== "Sortuj od Z do A"){
+            let sortedElements = this.state.elements.sort((a,b)=>{
+                if(b.title<a.title) return -1
+                else return 1
+            })
+            this.setState({elements: sortedElements})
+            this.setState({sortButton: "Sortuj od A do Z"})}
     }
 }
 
--- my-app/src/TodoItem.js
+++ my-app/src/TodoItem.js
@@ -1,17 +1,29 @@
 import React from 'react'
 
 class TodoItem extends React.Component {
+    state = {displayRemoveTask:'none'}
     render(){
         return(
             <div className='task'>
                 {this.props.title}
                 <button style={{marginLeft:'20px'}}>Edytuj</button>
-                <button style={{marginLeft:'20px'}} onClick={this.removeTask.bind(this)}>Usuń</button>
+                <button style={{marginLeft:'20px'}} onClick={this.areYouSure.bind(this)}>Usuń</button>
+                <div style={{display:this.state.displayRemoveTask }}>Czy na pewno chcesz usunąć?
+                    <button onClick={this.cancelClick.bind(this)}>Nie</button>
+                    <button onClick={this.removeTask.bind(this)}>Tak</button>
+                </div>
             </div>
         )
     }
     removeTask(){
+        this.setState({displayRemoveTask:'none'})
         this.props.removeTask(this.props.title)
+    }
+    areYouSure(){
+        this.setState({displayRemoveTask:'inline'})
+    }
+    cancelClick(){
+        this.setState({displayRemoveTask:'none'})
     }
 }
  
